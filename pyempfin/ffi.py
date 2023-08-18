859411485e8fdec5605c80c264dbf87a71c36140
Linxiao Francis Cong <flcong@outlook.com>
2023-08-17 21:19:07 -0400

    Add ffi12

--- pyempfin/ffi.py
+++ pyempfin/ffi.py
@@ -1370,3 +1370,88 @@ def getffi30(sic: int, getdesc=False) -> Union[int,pd._libs.missing.NAType]:
     else:
         return ffi30
 
+def getffi12(sic: int, getdesc=False) -> Union[int,pd._libs.missing.NAType]:
+    """Return Fama-French 12 industry code or description given SIC"""
+    if pd.isna(sic):
+        ffi12 = pd.NA
+        desc = ''
+    elif (100 <= sic <= 999) \
+            or (2000 <= sic <= 2399) \
+            or (2700 <= sic <= 2749) \
+            or (2770 <= sic <= 2799) \
+            or (3100 <= sic <= 3199) \
+            or (3940 <= sic <= 3989):
+        ffi12 = 1
+        desc = 'Consumer Nondurables -- Food, Tobacco, Textiles, Apparel, Leather, Toys'
+    elif (2500 <= sic <= 2519) \
+            or (2590 <= sic <= 2599) \
+            or (3630 <= sic <= 3659) \
+            or (3710 <= sic <= 3711) \
+            or (3714 <= sic <= 3714) \
+            or (3716 <= sic <= 3716) \
+            or (3750 <= sic <= 3751) \
+            or (3792 <= sic <= 3792) \
+            or (3900 <= sic <= 3939) \
+            or (3990 <= sic <= 3999):
+        ffi12 = 2
+        desc = 'Consumer Durables -- Cars, TVs, Furniture, Household Appliances'
+    elif (2520 <= sic <= 2589) \
+            or (2600 <= sic <= 2699) \
+            or (2750 <= sic <= 2769) \
+            or (3000 <= sic <= 3099) \
+            or (3200 <= sic <= 3569) \
+            or (3580 <= sic <= 3629) \
+            or (3700 <= sic <= 3709) \
+            or (3712 <= sic <= 3713) \
+            or (3715 <= sic <= 3715) \
+            or (3717 <= sic <= 3749) \
+            or (3752 <= sic <= 3791) \
+            or (3793 <= sic <= 3799) \
+            or (3830 <= sic <= 3839) \
+            or (3860 <= sic <= 3899):
+        ffi12 = 3
+        desc = 'Manufacturing -- Machinery, Trucks, Planes, Off Furn, Paper, Com Printing'
+    elif (1200 <= sic <= 1399) \
+            or (2900 <= sic <= 2999):
+        ffi12 = 4
+        desc = 'Oil, Gas, and Coal Extraction and Products'
+    elif (2800 <= sic <= 2829) \
+            or (2840 <= sic <= 2899):
+        ffi12 = 5
+        desc = 'Chemicals and Allied Products'
+    elif (3570 <= sic <= 3579) \
+        or (3660 <= sic <= 3692) \
+        or (3694 <= sic <= 3699) \
+        or (3810 <= sic <= 3829) \
+        or (7370 <= sic <= 7379):
+        ffi12 = 6
+        desc = 'Business Equipment -- Computers, Software, and Electronic Equipment'
+    elif (4800 <= sic <= 4899):
+        ffi12 = 7
+        desc = 'Telephone and Television Transmission'
+    elif (4900 <= sic <= 4949):
+        ffi12 = 8
+        desc = 'Utilities'
+    elif (5000 <= sic <= 5999) \
+            or (7200 <= sic <= 7299) \
+            or (7600 <= sic <= 7699):
+        ffi12 = 9
+        desc = 'Wholesale, Retail, and Some Services (Laundries, Repair Shops)'
+    elif (2830 <= sic <= 2839) \
+        or (3693 <= sic <= 3693) \
+        or (3840 <= sic <= 3859) \
+        or (8000 <= sic <= 8099):
+        ffi12 = 10
+        desc = 'Healthcare, Medical Equipment, and Drugs'
+    elif (6000 <= sic <= 6999):
+        ffi12 = 11
+        desc = 'Finance'
+    else:
+        ffi12 = 12
+        desc = 'Other -- Mines, Constr, BldMt, Trans, Hotels, Bus Serv, Entertainment'
+    if getdesc:
+        return desc
+    else:
+        return ffi12
+
+
